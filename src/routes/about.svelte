4393c33138cacb6089b4901245b080154887112b
Sergio Forés <sergiofores@gmail.com>
2020-05-17 21:50:23 +0200

    interactivity test

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -28,14 +28,21 @@
 
 <svelte:window bind:scrollY={y} />
 
-<h1 style="color: rgb({m.y},{m.x}, 255)">About this site</h1>
+<h1 style=" 
+	color: rgb({m.y},{m.x}, 255); 
+	opacity: {1 - Math.max(0, y / 40)};"
+>
+  About this site
+</h1>
 
 <p>This is the 'about' page. There's not much here.</p>
 <div on:mousemove={handleMousemove}>
   Pasa por encima de este texto para cambiar el color {m.x} x {m.y}
 </div>
 <div class="fixed">{y}</div>
-<h1>
+<h1 style="
+transform: translate({y}px,0)
+">
   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum soluta facilis,
   dolore dignissimos praesentium cumque repudiandae mollitia consequatur odit,
   dolorem iure molestiae deserunt? Fuga, repellat! Obcaecati ipsa temporibus
