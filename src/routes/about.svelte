b66583d5b1fb44745704dfd20017a7ced2c95140
Sergio Forés <sergiofores@gmail.com>
2020-05-17 21:39:19 +0200

    interactivity test

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,43 @@
+<script>
+  let m = { x: 0, y: 0 };
+
+  function handleMousemove(event) {
+    m.x = event.clientX;
+    m.y = event.clientY;
+  }
+
+  function detectaScroll(event) {
+    console.log(event);
+  }
+
+  let y;
+</script>
+
+<style lang="scss">
+  @import "../styles/main.scss";
+  .fixed {
+    position: fixed;
+    top: 0;
+    font-size: 3em;
+  }
+</style>
+
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<svelte:window bind:scrollY={y} />
+
+<h1 style="color: rgb({m.y},{m.x}, 255)">About this site</h1>
 
 <p>This is the 'about' page. There's not much here.</p>
+<div on:mousemove={handleMousemove}>
+  Pasa por encima de este texto para cambiar el color {m.x} x {m.y}
+</div>
+<div class="fixed">{y}</div>
+<h1>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum soluta facilis,
+  dolore dignissimos praesentium cumque repudiandae mollitia consequatur odit,
+  dolorem iure molestiae deserunt? Fuga, repellat! Obcaecati ipsa temporibus
+  necessitatibus aspernatur?
+</h1>
