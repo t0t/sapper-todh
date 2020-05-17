30b57441fac39b14f94c8251c480ae9e77e4c5ef
Sergio Forés <sergiofores@gmail.com>
2020-05-17 19:54:01 +0200

    init

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style lang="scss" global>
+ @import "./styles/main.scss";
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>XXXXX</title>
+</svelte:head>
+
+<h1>TODH!</h1>
