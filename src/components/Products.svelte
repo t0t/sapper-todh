64cf95fbb5fd7a39518a4f01b232a75380a8e1fc
Sergio Forés <sergiofores@gmail.com>
2020-05-18 14:09:37 +0200

    interactivity test

--- src/components/Products.svelte
+++ src/components/Products.svelte
@@ -1,36 +1,36 @@
 <script>
-	import { onMount } from "svelte";
+  import { onMount } from "svelte";
 
-    let data = [];
-    let productos = [];
+  let data = [];
+  let productos = [];
 
-	onMount(async () => {
-		const res = await fetch("db.json");
-        data = await res.json();
-        productos = data.productos;
-        console.log(productos);
-	});
+  onMount(async () => {
+    const res = await fetch("db.json");
+    data = await res.json();
+    productos = data.productos;
+    console.log(productos);
+  });
 </script>
 
 <style>
-	.photos {
-		width: 100%;
-		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		grid-gap: 8px;
-	}
+  .photos {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 8px;
+  }
 </style>
 
 <h1>Photo album</h1>
 
 <div class="photos">
-	{#each productos as producto}
-		<figure>
-			<img src={producto.imagen} alt={producto.description}>
-			<figcaption>{producto.name}</figcaption>
-		</figure>
-	{:else}
-		<!-- this block renders when photos.length === 0 -->
-		<p>loading...</p>
-	{/each}
+  {#each productos as producto}
+    <figure>
+      <img src={producto.imagen} alt={producto.description} />
+      <figcaption>{producto.name}</figcaption>
+    </figure>
+  {:else}
+    <!-- this block renders when photos.length === 0 -->
+    <p>loading...</p>
+  {/each}
 </div>
